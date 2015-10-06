dbf87d7feb63b46cba06bad12aa57e813515a7dc
Youta Seki <uyu9480s@outlook.jp>
2015-10-07 05:06:42 +0900

    fixed : dotfilesのパスを取得, 文字コードをsjisに変更

--- run_mklink.bat
+++ run_mklink.bat
@@ -1,17 +1,17 @@
 @echo off
 
-SET DOTPATH=%HOMEPATH%\Documents\GitHub\dotfiles
+SET DOTPATH=%~dp0
 openfiles > NUL 2>&1 
 if NOT %ERRORLEVEL% EQU 0 goto NotAdmin
 mklink /D %HOMEPATH%\.vim %DOTPATH%\.vim
 mklink %HOMEPATH%\.vimrc %DOTPATH%\.vimrc
 mklink %HOMEPATH%\.gvimrc %DOTPATH%\.gvimrc
 echo %DOTPATH%
-echo へのシンボリックリンクを%HOMEPATH%に作成しました
+echo �ւ̃V���{���b�N�����N��%HOMEPATH%�ɍ쐬���܂���
 TIMEOUT /T -1
 goto End
  
 :NotAdmin 
-echo 管理者権限で実行してください
+echo �Ǘ��Ҍ����Ŏ��s���Ă�������
 TIMEOUT /T -1
 :End
